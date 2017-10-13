e809044a79937af553e522c98cf5dc3d63009941
Дмитрий Султанов <d.sultanov@infra.loc>
2017-10-13 15:33:24 +0300

    w9

--- 5_basket.php
+++ 5_basket.php
@@ -6,7 +6,7 @@
 	<? include('inc/head.php'); ?>
 </head>
 <body>
-	<div class="ordering-screen"></div>
+	<div class="basket-screen"></div>
 	<div class="wrapper">
 		<? include('inc/header.php'); ?>
 		<div class="container">
@@ -32,13 +32,76 @@
 									<p>ДИО 1.152</p>
 								</div>
 							</td>
+							<td>
+								<div class="good-price">
+									<div class="current-price"><span>22 500</span> р.</div>
+									<div class="old-price"><span>26 500</span> р.</div>
+								</div>
+							</td>
+							<td>
+								<div class="enter_num">
+									<button class="minus"></button>
+									<input class="input_num" maxlength="2" type="text" value="1">
+									<button class="plus"></button>
+								</div>
+							</td>
+							<td>
+								<div class="good-price">
+									<div class="current-price total"><span>45 000 р.</span></div>
+								</div>
+							</td>
+							<td>
+								<button type="button" class="delete-good"></button>
+							</td>
+						</tr>
+						<tr>
+							<td><img src="templates/img/basket-good2.jpg" alt=""/></td>
+							<td>
+								<div class="good-name">
+									<p><strong>Карусель вертушка</strong></p>
+									<p>ДИО 1.152</p>
+								</div>
+							</td>
+							<td>
+								<div class="good-price">
+									<div class="current-price"><span>22 500</span> р.</div>
+									<div class="old-price"><span>26 500</span> р.</div>
+								</div>
+							</td>
+							<td>
+								<div class="enter_num">
+									<button class="minus"></button>
+									<input class="input_num" maxlength="2" type="text" value="1">
+									<button class="plus"></button>
+								</div>
+							</td>
+							<td>
+								<div class="good-price">
+									<div class="current-price total"><span>25 500 р.</span></div>
+								</div>
+							</td>
+							<td>
+								<button type="button" class="delete-good"></button>
+							</td>
+						</tr>
+						<tr>
 							<td></td>
 							<td></td>
 							<td></td>
+							<td>
+								<div class="total">Итого:</div>
+							</td>
+							<td>
+								<div class="total total-sum"><span>67 500</span> р.</div>
+							</td>
 							<td></td>
 						</tr>
 					</tbody>
 				</table>
+			</div>
+			<div class="after-goods-btns flex jcsb">
+				<a href="#" class="border-btn"><img src="templates/img/blue-arrow-left.png" alt=""/> Назад в каталог</a>
+				<a href="#" class="btn btn-large noshadow">Оформить заказ</a>
 			</div>
 		</div>
 		<div class="blue-panel answer-questions">
