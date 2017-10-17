fb5cefd70fa10eee1be1f04d9e12a869127b9ffc
Дмитрий Султанов <d.sultanov@infra.loc>
2017-10-17 17:51:03 +0300

    w10

--- 6_list_news.php
+++ 6_list_news.php
@@ -7,14 +7,14 @@
 </head>
 <body>
 	<div class="list-news-screen"></div>
-	<div class="wrapper">
+	<div class="wrapper news-wrapper">
 		<? include('inc/header.php'); ?>
 		<div class="container">
 			<h1>Новости</h1>
-			<div class="w-panel-sides large-sides news-wrapper list-news">
-				<div class="w-large">
+			<div class="w-panel-sides large-sides list-news">
+				<div class="w-large list-news-items">
 					<div class="news-items flex jcsb">
-						<div class="news-item">
+						<div class="news-item on">
 			            	<a href="#"></a>
 			            	<div class="slide-desc">
 			            		<p class="good-name">Проведена спец оценка предприятия</p>
@@ -22,7 +22,7 @@
 			            		<div class="tac"><a href="#" class="circle-btn add"></a></div>
 			            	</div>
 			            </div>
-			            <div class="news-item">
+			            <div class="news-item on">
 			            	<a href="#"></a>
 			            	<div class="slide-desc">
 			            		<p class="good-name">В Иркутске прошла «Байкальская строительная неделя».</p>
@@ -30,7 +30,7 @@
 			            		<div class="tac"><a href="#" class="circle-btn add"></a></div>
 			            	</div>
 			            </div>
-			            <div class="news-item">
+			            <div class="news-item on">
 			            	<a href="#"></a>
 			            	<div class="slide-desc">
 			            		<p class="good-name">В Иркутске прошла «Байкальская строительная неделя».</p>
@@ -38,7 +38,7 @@
 			            		<div class="tac"><a href="#" class="circle-btn add"></a></div>
 			            	</div>
 			            </div>
-			            <div class="news-item">
+			            <div class="news-item on">
 			            	<a href="#"></a>
 			            	<div class="slide-desc">
 			            		<p class="good-name">Проведена спец оценка предприятия</p>
@@ -46,7 +46,7 @@
 			            		<div class="tac"><a href="#" class="circle-btn add"></a></div>
 			            	</div>
 			            </div>
-			            <div class="news-item">
+			            <div class="news-item on">
 			            	<a href="#"></a>
 			            	<div class="slide-desc">
 			            		<p class="good-name">В Иркутске прошла «Байкальская строительная неделя».</p>
@@ -54,7 +54,7 @@
 			            		<div class="tac"><a href="#" class="circle-btn add"></a></div>
 			            	</div>
 			            </div>
-			            <div class="news-item">
+			            <div class="news-item on">
 			            	<a href="#"></a>
 			            	<div class="slide-desc">
 			            		<p class="good-name">Проведена спец оценка предприятия</p>
@@ -62,30 +62,14 @@
 			            		<div class="tac"><a href="#" class="circle-btn add"></a></div>
 			            	</div>
 			            </div>
-			            <div class="news-item">
-			            	<a href="#"></a>
-			            	<div class="slide-desc">
-			            		<p class="good-name">Проведена спец оценка предприятия</p>
-			            		<p><span>12.07.2017</span></p>
-			            		<div class="tac"><a href="#" class="circle-btn add"></a></div>
-			            	</div>
-			            </div>
-			            <div class="news-item">
-			            	<a href="#"></a>
-			            	<div class="slide-desc">
-			            		<p class="good-name">Проведена спец оценка предприятия</p>
-			            		<p><span>12.07.2017</span></p>
-			            		<div class="tac"><a href="#" class="circle-btn add"></a></div>
-			            	</div>
-			            </div>
-			            <div class="news-item">
-			            	<a href="#"></a>
-			            	<div class="slide-desc">
-			            		<p class="good-name">Проведена спец оценка предприятия</p>
-			            		<p><span>12.07.2017</span></p>
-			            		<div class="tac"><a href="#" class="circle-btn add"></a></div>
-			            	</div>
-			            </div>
+					</div>
+					<div class="news-items blocks flex jcsb">
+						<div class="news-item blocks__block" id="1"></div>
+						<div class="news-item blocks__block" id="2"></div>
+						<div class="news-item blocks__block" id="3"></div>
+						<div class="news-item blocks__block" id="4"></div>
+						<div class="news-item blocks__block" id="5"></div>
+						<div class="news-item blocks__block" id="6"></div>
 					</div>
 					<div class="btn-wrap">
 						<a href="#" class="border-btn">Показать еще</a>
@@ -113,6 +97,280 @@
 				</div>
 			</div>
 		</div>
+
+		<ul class="news-content">
+			<li class="news-content__content">
+				<div class="container">
+					<div class="w-panel-sides news-wrapper">
+						<div class="w-small">
+							<aside>
+								<div class="tac">
+									<div class="date">08.07.17</div>
+								</div>
+							</aside>
+						</div>
+						<div class="w-large">
+							<div class="news">
+								<h1>Выставка в Германии GalaBau</h1>
+								<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+								Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала 
+								XVI века.</p>
+								<p>В то время некий безымянный печатник создал большую коллекцию размеров и 
+								форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не 
+								только успешно пережил без заметных изменений пять веков, но и перешагнул в 
+								электронный дизайн.</p>
+								<div class="news-icons flex jcsb">
+									<div class="n-ico"><img src="templates/img/news_ico1.jpg" alt=""/></div>
+									<div class="n-ico"><img src="templates/img/news_ico1.jpg" alt=""/></div>
+								</div>
+								<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+								Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала 
+								XVI века.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="blue-back-btn">
+					<a href="#"></a>
+					<div class="flex center align">
+						<div>
+							<span>д</span>
+							<span>а</span>
+							<span>з</span>
+							<span>а</span>
+							<span>н</span>
+							<i class="arrow"><img src="templates/img/scrollup-arrow.png" alt=""/></i>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="news-content__content">
+				<div class="container">
+					<div class="w-panel-sides news-wrapper">
+						<div class="w-small">
+							<aside>
+								<div class="tac">
+									<div class="date">08.07.17</div>
+								</div>
+							</aside>
+						</div>
+						<div class="w-large">
+							<div class="news">
+								<h1>Выставка в Германии GalaBau</h1>
+								<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+								Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала 
+								XVI века.</p>
+								<p>В то время некий безымянный печатник создал большую коллекцию размеров и 
+								форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не 
+								только успешно пережил без заметных изменений пять веков, но и перешагнул в 
+								электронный дизайн.</p>
+								<div class="news-icons flex jcsb">
+									<div class="n-ico"><img src="templates/img/news_ico1.jpg" alt=""/></div>
+									<div class="n-ico"><img src="templates/img/news_ico1.jpg" alt=""/></div>
+								</div>
+								<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+								Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала 
+								XVI века.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="blue-back-btn">
+					<a href="#"></a>
+					<div class="flex center align">
+						<div>
+							<span>д</span>
+							<span>а</span>
+							<span>з</span>
+							<span>а</span>
+							<span>н</span>
+							<i class="arrow"><img src="templates/img/scrollup-arrow.png" alt=""/></i>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="news-content__content">
+				<div class="container">
+					<div class="w-panel-sides news-wrapper">
+						<div class="w-small">
+							<aside>
+								<div class="tac">
+									<div class="date">08.07.17</div>
+								</div>
+							</aside>
+						</div>
+						<div class="w-large">
+							<div class="news">
+								<h1>Выставка в Германии GalaBau</h1>
+								<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+								Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала 
+								XVI века.</p>
+								<p>В то время некий безымянный печатник создал большую коллекцию размеров и 
+								форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не 
+								только успешно пережил без заметных изменений пять веков, но и перешагнул в 
+								электронный дизайн.</p>
+								<div class="news-icons flex jcsb">
+									<div class="n-ico"><img src="templates/img/news_ico1.jpg" alt=""/></div>
+									<div class="n-ico"><img src="templates/img/news_ico1.jpg" alt=""/></div>
+								</div>
+								<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+								Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала 
+								XVI века.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="blue-back-btn">
+					<a href="#"></a>
+					<div class="flex center align">
+						<div>
+							<span>д</span>
+							<span>а</span>
+							<span>з</span>
+							<span>а</span>
+							<span>н</span>
+							<i class="arrow"><img src="templates/img/scrollup-arrow.png" alt=""/></i>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="news-content__content">
+				<div class="container">
+					<div class="w-panel-sides news-wrapper">
+						<div class="w-small">
+							<aside>
+								<div class="tac">
+									<div class="date">08.07.17</div>
+								</div>
+							</aside>
+						</div>
+						<div class="w-large">
+							<div class="news">
+								<h1>Выставка в Германии GalaBau</h1>
+								<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+								Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала 
+								XVI века.</p>
+								<p>В то время некий безымянный печатник создал большую коллекцию размеров и 
+								форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не 
+								только успешно пережил без заметных изменений пять веков, но и перешагнул в 
+								электронный дизайн.</p>
+								<div class="news-icons flex jcsb">
+									<div class="n-ico"><img src="templates/img/news_ico1.jpg" alt=""/></div>
+									<div class="n-ico"><img src="templates/img/news_ico1.jpg" alt=""/></div>
+								</div>
+								<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+								Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала 
+								XVI века.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="blue-back-btn">
+					<a href="#"></a>
+					<div class="flex center align">
+						<div>
+							<span>д</span>
+							<span>а</span>
+							<span>з</span>
+							<span>а</span>
+							<span>н</span>
+							<i class="arrow"><img src="templates/img/scrollup-arrow.png" alt=""/></i>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="news-content__content">
+				<div class="container">
+					<div class="w-panel-sides news-wrapper">
+						<div class="w-small">
+							<aside>
+								<div class="tac">
+									<div class="date">08.07.17</div>
+								</div>
+							</aside>
+						</div>
+						<div class="w-large">
+							<div class="news">
+								<h1>Выставка в Германии GalaBau</h1>
+								<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+								Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала 
+								XVI века.</p>
+								<p>В то время некий безымянный печатник создал большую коллекцию размеров и 
+								форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не 
+								только успешно пережил без заметных изменений пять веков, но и перешагнул в 
+								электронный дизайн.</p>
+								<div class="news-icons flex jcsb">
+									<div class="n-ico"><img src="templates/img/news_ico1.jpg" alt=""/></div>
+									<div class="n-ico"><img src="templates/img/news_ico1.jpg" alt=""/></div>
+								</div>
+								<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+								Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала 
+								XVI века.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="blue-back-btn">
+					<a href="#"></a>
+					<div class="flex center align">
+						<div>
+							<span>д</span>
+							<span>а</span>
+							<span>з</span>
+							<span>а</span>
+							<span>н</span>
+							<i class="arrow"><img src="templates/img/scrollup-arrow.png" alt=""/></i>
+						</div>
+					</div>
+				</div>
+			</li>
+			<li class="news-content__content">
+				<div class="container">
+					<div class="w-panel-sides news-wrapper">
+						<div class="w-small">
+							<aside>
+								<div class="tac">
+									<div class="date">08.07.17</div>
+								</div>
+							</aside>
+						</div>
+						<div class="w-large">
+							<div class="news">
+								<h1>Выставка в Германии GalaBau</h1>
+								<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+								Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала 
+								XVI века.</p>
+								<p>В то время некий безымянный печатник создал большую коллекцию размеров и 
+								форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не 
+								только успешно пережил без заметных изменений пять веков, но и перешагнул в 
+								электронный дизайн.</p>
+								<div class="news-icons flex jcsb">
+									<div class="n-ico"><img src="templates/img/news_ico1.jpg" alt=""/></div>
+									<div class="n-ico"><img src="templates/img/news_ico1.jpg" alt=""/></div>
+								</div>
+								<p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. 
+								Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала 
+								XVI века.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="blue-back-btn">
+					<a href="#"></a>
+					<div class="flex center align">
+						<div>
+							<span>д</span>
+							<span>а</span>
+							<span>з</span>
+							<span>а</span>
+							<span>н</span>
+							<i class="arrow"><img src="templates/img/scrollup-arrow.png" alt=""/></i>
+						</div>
+					</div>
+				</div>
+			</li>
+		</ul>
+
 		<div class="blue-panel answer-questions">
 			<div class="container">
 				<div class="aq-wrap flex align">
@@ -129,5 +387,96 @@
 	<? include('inc/footer.php'); ?>
 	<? include('inc/modal.php'); ?>
 	<? include('inc/scripts.php'); ?>
+
+	<script>
+		$(document).ready(function(){
+			block = $('.news-item.blocks__block');
+			bname = $('.news-item.on');
+			content = $('.news-content__content');
+			closeBtn = $('.blue-back-btn');
+			wHeight = $(window).outerHeight();
+			wWidth = $(window).outerWidth();
+			bHeight = block.outerHeight();
+			bWidth = block.outerWidth();
+			xVal = Math.round(wWidth / bWidth) + 0.03;
+			yVal = wHeight / bHeight + 0.03;
+
+			expand = function() {
+			  var aBlock, num;
+			  num = $(this).index();
+			  aBlock = block.eq(num);
+			  $('body').addClass('oh');
+			  if (!aBlock.hasClass('active')) {
+			    bname.css('opacity', '0');
+			    aBlock.css({
+			      'transform': 'scale(' + xVal + ',' + yVal + ')',
+			      '-webkit-transform': 'scale(' + xVal + ',' + yVal + ')'
+			    });
+			    aBlock.addClass('active');
+			    openContent(num);
+			    $('.news-content').css('z-index', '22');
+
+			    return false
+			  }
+			};
+
+			openContent = function(num) {
+			  var aContent;
+			  content.css({
+			    'transition': 'all 0.3s 0.4s ease-out',
+			    '-webkit-transition': 'all 0.3s 0.4s ease-out'
+			  });
+			  aContent = content.eq(num);
+			  aContent.addClass('active');
+			};
+
+			closeContent = function() {
+			  bname.css('opacity', '1');
+			  content.css({
+			    'transition': 'all 0.1s 0 ease-out',
+			    '-webkit-transition': 'all 0.1s 0 ease-out'
+			  });
+			  block.css({
+			    'transform': 'scale(1)',
+			    '-webkit-transform': 'scale(1)'
+			  });
+			  block.removeClass('active');
+			  content.removeClass('active');
+			  $('body').removeClass('oh');
+			  $('.news-content').css('z-index', '-1');
+
+			  return false
+			};
+
+			updateValues = function() {
+			  var yVal;
+			  var xVal;
+			  var bWidth;
+			  var bHeight;
+			  var wWidth;
+			  var wHeight;
+			  var aBlock;
+			  if (block.hasClass('active')) {
+			    aBlock = $('.blocks__block.active');
+			    wHeight = $(window).height();
+			    wWidth = $(window).width();
+			    bHeight = block.height();
+			    bWidth = block.width();
+			    xVal = Math.round(wWidth / bWidth) + 0.03;
+			    yVal = wHeight / bHeight + 0.03;
+			    aBlock.css({
+			      'transform': 'scale(' + xVal + ',' + yVal + ')',
+			      '-webkit-transform': 'scale(' + xVal + ',' + yVal + ')'
+			    });
+			  }
+			};
+
+			$(window).on('resize', updateValues);
+
+			bname.on('click', expand);
+
+			closeBtn.on('click', closeContent);
+		});
+	</script>
 </body>
 </html>
